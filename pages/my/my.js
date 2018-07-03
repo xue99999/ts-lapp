f7f5c659a99a876d4a1b74852d78350d5b1ef2ec
dongqingfeng <qingfeng.dong@tfcpay.com>
2018-07-03 12:34:01 +0800

    我的页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,124 @@
+var app=getApp();
+
 Page({
 
   /**
+   * 
    * 页面的初始数据
    */
   data: {
-    name:"是是是",
-    phone:"1355200665"
+
+    nickName: "",
+    phone: "",
+    avatarUrl: "",
+    routers: [{
+        name: '辣妈经期',
+        url: '',
+        icon: '../img/mom1@3x.png',
+        code: '10'
+      },
+      {
+        name: '经期与排卵',
+        url: '',
+        icon: '../img/collection@3x.png',
+        code: '11'
+      },
+      {
+        name: '我的课程',
+        url: '',
+        icon: '../img/course@3x.png',
+        code: '10'
+      },
+      {
+        name: '我的收藏',
+        icon: '../img/collection@3x.png',
+        code: '11'
+      },
+      {
+        name: '建议反馈',
+        url: '/pages/Course/course',
+        icon: '../img/feedback@3x.png',
+        code: '10'
+      }
+
+    ]
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+    var that = this;
+    wx.request({
+      url: 'http://192.168.9.171:8080/app/mock/20//owner/query', //仅为示例，并非真实的接口地址
+      success(res) {
+        console.log(res);
+        that.setData({
+          nickName: res.data.nickName,
+          phone: res.data.phone,
+          avatarUrl: res.data.avatarUrl,
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
+
   },
+
+
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
+  },
+  /**
+   * 绑定手机号
+   */
+  onClickPhone() {
+    console.log('这里是绑定手机号')
   }
+
 })
