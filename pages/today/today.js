56c5823e7741325705df4fd5fc028a8d7f6beae4
李何佳 <hejia.li@jrtpay.com>
2018-07-13 15:13:12 +0800

    add

--- pages/today/today.js
+++ pages/today/today.js
@@ -17,6 +17,17 @@ Page({
     today:moment().format('YYYY-MM-DD'),
     currentDay:{},
     formatDay:null,
+    // 圆圈显示数据
+    showObj:{
+      // 模式辣妈
+        "02":{
+          babyText:'宝宝信息显示'
+        },
+        // 模式少女
+        "01":{
+
+        }
+    }
   },
   clickArr:function(e){
     console.log(e.currentTarget.dataset.index)
@@ -31,37 +42,54 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const day=moment().format("YYYY-MM-DD");
-     //const endDay = moment().add(5,'days').format("YYYY-MM-DD");
-    //console.log(day,endDay)
-       this.setData({
-         today:moment().format('D'),
-         days:currentWeek(),
-         formatDay: day
-        })
+    
+    const day = moment().format("YYYY-MM-DD");
+    const today=moment().format('D');
+    this.setData({
+      today,
+      days: currentWeek(),
+      formatDay: day
+    })
 
-       var query = {
-         startDay: day,
-         endDay: day
-       }
+    this.query(day, day, today);
+  },
+ /**
+  * startDay 开始时间
+  * endDay
+  * currentDay 当前天数据
+  */
+  query:function (startDay,endDay,currentDay){
+   
+    var query = {
+      startDay,
+      endDay
+    }
 
     userInfoQueryBodyStatus(query).then(res => {
-      const {list} = res;
-      app.globalData.bodyStatus=list;
+      const { list, userModel } = res;
+      app.globalData.bodyStatus = list;
 
-        for (let i = 0; i < list.length;i++){
-           const dy=list[i];
-           if(dy.day===day){
-                this.setData({
-                  currentDay:dy
-                })
-           }
-         }
-        console.log(this.data.currentDay)
-        var inner = this.data.currentDay
-       
-       })
-      
+      for (let i = 0; i < list.length; i++) {
+        const dy = list[i];
+        if (dy.day === currentDay) {
+          let showObj={}
+          // 处理当前需要显示的数据
+          if (userModel==='01'){
+            showObj['01']={
+              babyText: `宝宝${dy.babyMonth}月`
+            }
+          }else{
+
+          }
+          this.setData({
+            userModel,
+            currentDay: dy
+          })
+          
+        }
+      }
+     
+    })
   },
 
   /**
