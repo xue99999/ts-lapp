cbb554f775dee23107669333675c7763fe0d259c
dongqingfeng <qingfeng.dong@tfcpay.com>
2018-07-16 15:25:43 +0800

    111

--- service/user.js
+++ service/user.js
@@ -2,19 +2,17 @@ import Http from '../utils/http.js';
 import mock from './mock.js';
 //RAP2 开发阶段测试路径
 //const baseUrl = 'http://192.168.9.171:8080/app/mock/20/';
-
 //程东林 本地测试路径
 //const baseUrl ='http://192.168.5.137:8081/ts-app-web/';
 //王丽娟 本地测试路径
 //const baseUrl = 'http://192.168.5.82:8080/ts-app-web/';
-
 // const baseUrl ='http://192.168.5.82:8080/ts-app-web/';
 //李康 本地测试路径
- const baseUrl ='http://192.168.5.167:8080/ts-app-web/'
+ //const baseUrl ='http://192.168.5.167:8080/ts-app-web/'
 //刘小东 本地测试路径
-
 //const baseUrl = 'http://192.168.5.113:8083/ts-app-web/'
-
+//171测试服务器
+const baseUrl ='http://192.168.9.171:8089/'
 const {
   get
 } = Http;
