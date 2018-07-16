e5176f8ec7dca1b5b7cde963c640e4bd7a0286f8
李何佳 <hejia.li@jrtpay.com>
2018-07-16 17:49:41 +0800

    no message

--- service/user.js
+++ service/user.js
@@ -8,7 +8,7 @@ import Http from '../utils/http.js';
 //const baseUrl = 'http://192.168.5.82:8080/ts-app-web/';
 // const baseUrl ='http://192.168.5.82:8080/ts-app-web/';
 //李康 本地测试路径
- const baseUrl ='http://192.168.5.167:8080/ts-app-web/'
+ //const baseUrl ='http://192.168.5.167:8080/ts-app-web/'
 //刘小东 本地测试路径
 
 // const baseUrl = 'http://192.168.5.113:8083/ts-app-web/'
