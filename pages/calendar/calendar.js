3cc6d4f9dd0034477d7c7e2d947e1f3112ba025c
jiafusheng <fusheng.jia@jrtpay.com>
2018-07-18 15:19:34 +0800

    添加分享

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -1229,5 +1229,27 @@ Page({
     }
 
     return false;
+  },
+    onShareAppMessage: function (options) {
+    　　var that = this;
+    　　// 设置菜单中的转发按钮触发转发事件时的转发内容
+    　　var shareObj = {
+      　　　　title: "她师",        // 默认是小程序的名称(可以写slogan等)
+      　　　　path: '/pages/today/today',        // 默认是当前页面，必须是以‘/’开头的完整路径
+      　　　　imgUrl: '',     //自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径，支持PNG及JPG，不传入 imageUrl 则使用默认截图。显示图片长宽比是 5:4
+      　　　　success: function (res) {
+        　　　　　　// 转发成功之后的回调
+        　　　　　　if (res.errMsg == 'shareAppMessage:ok') {
+        　　　　　　}
+      　　　　},
+      　　　　fail: function () {
+        　　　　　　// 转发失败之后的回调
+        　　　　　　if (res.errMsg == 'shareAppMessage:fail cancel') {
+          　　　　　　　　// 用户取消转发
+        　　　　　　} else if (res.errMsg == 'shareAppMessage:fail') {
+          　　　　　　　　// 转发失败，其中 detail message 为详细失败信息
+        　　　　　　}
+      　　　　}
+    　　}
   }
 })
