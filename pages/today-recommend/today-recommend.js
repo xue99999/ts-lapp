112b0bcbb7331f109da01dfacf1fbebca92e29de
jiafusheng <fusheng.jia@jrtpay.com>
2018-07-31 11:41:26 +0800

    改 页面 固定不动

--- pages/today-recommend/today-recommend.js
+++ pages/today-recommend/today-recommend.js
@@ -16,111 +16,109 @@ Page({
    */
   data: {
     indicatorDots: false,
-    autoplay: false,
+    autoplay: true,
     interval: 5000,
     duration: 1000,
     list: [],
     url: '../course-details/course-details',
-    pos:0,
+    pos: 0,
     lists: [[
-        "../img/taichong.jpg",
-         '../img/shaoze.jpg',
-         '../img/hegu.jpg'
-        ],
-      [
-        "../img/sanyin.jpg",
-      ],
-      [
-      ],
-      [
-        "../img/zusan.jpg",
-        '../img/xuehai.jpg'
-      ]
+      "../img/taichong.jpg",
+      '../img/shaoze.jpg',
+      '../img/hegu.jpg'
+    ],
+    [
+      "../img/sanyin.jpg",
+    ],
+    [
+    ],
+    [
+      "../img/zusan.jpg",
+      '../img/xuehai.jpg'
+    ]
     ],
 
   },
 
-/**
- * 生命周期函数--监听页面加载
- */
-onLoad: function(options) {
-  auth();
-  const day = moment().format("YYYY-MM-DD");
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    auth();
+    const day = moment().format("YYYY-MM-DD");
 
-  var list = app.globalData.bodyStatus;
-  console.log(list)
-  for (let i = 0; i < list.length; i++) {
-    const dy = list[i];
-    if (dy.day === day) {
-      console.log(  list[i].physiologicalCycle);
-      var pos;
-    
-      if (list[i].physiologicalCycle==="05"){
-        pos=0;
-      }
-      if (list[i].physiologicalCycle === "02") {
-        pos = 1;
-      }
-      if (list[i].physiologicalCycle === "03") {
-        pos = 2;
-      }
-      if (list[i].physiologicalCycle === "01") {
-        pos = 3;
-      }
-      if (list[i].physiologicalCycle === "04") {
-        pos = 2;
-      }
-      this.setData({
-        pos: pos
-      })
-    }
-  }
+    var list = app.globalData.bodyStatus;
+    console.log(list)
+    for (let i = 0; i < list.length; i++) {
+      const dy = list[i];
+      if (dy.day === day) {
+        console.log(list[i].physiologicalCycle);
+        var pos;
 
-
-
-  this.getData(page);
-},
-
-getData: function(page) {
-  var data = {
-    page: page,
-    rows: 20
-  }
-  apiCourseRecommendList(data).then(result => {
-    console.log('今日推荐', result);
-    var list = this.data.list;
-    if (result.code === 200) {
-      if (page > 1) {
-        for (var i = 0; i < result.list.length; i++) {
-          list.push(result.list[i]);
+        if (list[i].physiologicalCycle === "05") {
+          pos = 3;
         }
-      } else {
-        list = [];
-        list = result.list;
+        if (list[i].physiologicalCycle === "02") {
+          pos = 1;
+        }
+        if (list[i].physiologicalCycle === "03") {
+          pos = 2;
+        }
+        if (list[i].physiologicalCycle === "01") {
+          pos = 0;
+        }
+        if (list[i].physiologicalCycle === "04") {
+          pos = 2;
+        }
+        this.setData({
+          pos: pos
+        })
       }
-      this.setData({
-        list: list,
-      })
-    } else {
-      getApp().getShouHint();
     }
-  })
 
-},
 
-/**
- * 页面相关事件处理函数--监听用户下拉动作
- */
-onPullDownRefresh: function() {
-  this.getData(1);
-},
 
-/**
- * 页面上拉触底事件的处理函数
- */
-onReachBottom: function() {
-  page += 1;
-  this.getData(page)
-}
+    this.getData(page);
+  },
+
+  getData: function (page) {
+    var data = {
+      page: page,
+      rows: 20
+    }
+    apiCourseRecommendList(data).then(result => {
+      console.log('今日推荐', result);
+      var list = this.data.list;
+      if (result.code === 200) {
+        if (page > 1) {
+          for (var i = 0; i < result.list.length; i++) {
+            list.push(result.list[i]);
+          }
+        } else {
+          list = [];
+          list = result.list;
+        }
+        this.setData({
+          list: list,
+        })
+      }
+    })
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    this.getData(1);
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    page += 1;
+    this.getData(page)
+  }
 
 })
